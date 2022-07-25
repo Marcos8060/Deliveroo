41ea1f0b86f0eabec5a48f8f578e022026a07537
Marcos8060 <marcosgav80@gmail.com>
2022-07-25 18:44:45 +0300

    created the home hero section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
+import burger from '../assets/Images/burger2.png'
 
 const Home = () => {
   return (
-    <div className='home--background h-screen p-4'>
+    <div className='home--background h-screen p-4 mx-auto'>
         <Navigation />
+        <div className='container mx-auto px-4 md:flex sm:grid'>
+            <img className='object-cover w-8/12 sm:w-12/12' src={burger} alt="burger" />
+            <h1 className='text-white lg:text-8xl font-bold my-40 sm:hidden'>Delicious Burgers.</h1>
+        </div>
     </div>
   )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { MdShoppingBasket } from 'react-icons/md'
 import { HiUserCircle } from 'react-icons/hi'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
   return (
     <div className='bg-white h-14 w-11/12 mx-auto rounded-2xl px-4 md:flex justify-between items-center'>
         <div>
-          <h1 className='font-bold text-4xl'>Burger</h1>
+          <h1 className='font-bold text-4xl'>Gomoto</h1>
         </div>
         <ul className='md:flex items-center justify-between'>
-            <li className='m-4 font-semibold'>Home</li>
-            <li className='m-4 font-semibold'>About</li>
-            <li className='m-4 font-semibold'>Deliveries</li>
+            <li className='m-4 font-semibold'>
+                <Link to='/'>Home</Link>
+            </li>
+            <li className='m-4 font-semibold'>
+                <Link to='/about'>About</Link>
+            </li>
+            <li className='m-4 font-semibold'>
+                <Link to='/deliveries'>Deliveries</Link>
+            </li>
         </ul>
         <div className='flex items-center justify-between'>
             <MdShoppingBasket className='text-2xl m-4'/>
