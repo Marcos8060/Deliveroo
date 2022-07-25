287e464d81b2ab95672d0e31711b51bbeeae6dc3
Marcos8060 <marcosgav80@gmail.com>
2022-07-25 17:49:49 +0300

    installed react icons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
+
 const Navigation = () => {
   return (
     <div className='bg-white h-14 w-11/12 mx-auto rounded-2xl px-4 md:flex justify-between items-center'>
         <div>
           <h1 className='font-bold text-4xl'>Burger</h1>
         </div>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Deliveries</li>
+        <ul className='md:flex items-center justify-between'>
+            <li className='m-4'>Home</li>
+            <li className='m-4'>About</li>
+            <li className='m-4'>Deliveries</li>
         </ul>
     </div>
   )
