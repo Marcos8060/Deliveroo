666692ecd5e9b69b28d4f74db08d919a29257d32
Marcos8060 <marcosgav80@gmail.com>
2022-07-25 22:00:32 +0300

    added the products to the carousel

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,42 +1,51 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import burger from '../assets/Images/burger3.png'
 
+
+import Slider from "react-slick";
+// REACT SLICK
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+
+
 const Products = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      };
+
   return (
-    <div>
+    <div className='container'>
         <h3 className='text-center text-yellow font-bold mt-20'>Quick Pick</h3>
         <h1 className='text-center text-black font-bold text-6xl'>Popular Goods</h1>
-        <ul className='flex justify-center items-center m-4'>
-            <li className='m-4 font-bold'>
-                <Link to='/all'>All</Link>
-            </li>
-            <li className='m-4 font-bold'>
-                <Link to='/burger'>Burger</Link>
-            </li>
-            <li className='m-4 font-bold'>
-                <Link to='/pizza'>Hot Pizza</Link>
-            </li>
-        </ul>
-        <div className='flex items-center justify-center mx-auto'>
-            <div className='mx-auto text-center'>
+        
+        <Slider {...settings}>
+           <div className='text-center'>
               <img className='object-fit' src={burger} alt="burger" />
               <span className='text-center font-bold'>Pepperoni</span>
               <small className='block text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, saepe.</small>
             </div>
-            <div className='mx-auto text-center'>
+            <div className='text-center'>
               <img className='object-fit' src={burger} alt="burger" />
-              <span className='text-center'>Pepperoni</span>
+              <span className='text-center font-bold'>Pepperoni</span>
+              <small className='block'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, voluptatibus?</small>
             </div>
-            <div className='mx-auto text-center'>
+            <div className='text-center'>
               <img className='object-fit' src={burger} alt="burger" />
-              <span className='text-center'>Pepperoni</span>
+              <span className='text-center font-bold'>Pepperoni</span>
+              <small className='block'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, sapiente?</small>
             </div>
-            <div className='mx-auto text-center'>
+            <div>
               <img className='object-fit' src={burger} alt="burger" />
-              <span className='text-center'>Pepperoni</span>
+              <span className='text-center font-bold'>Pepperoni</span>
+              <small className='block'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, non.</small>
             </div>
-        </div>
+        </Slider>
     </div>
   )
 }
