64258c070f1dbc4ec10d5b5f422d25998fa3f73c
Marcos8060 <marcosgav80@gmail.com>
2022-07-26 22:24:35 +0300

    polished the soon section

--- src/components/Soon.js
+++ src/components/Soon.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import burger from '../assets/Images/burger2.png'
+import burger from '../assets/Images/burger7.png'
 
 
 const Soon = () => {
   return (
     <div className='bg-yellow my-40 flex items-center justify-between p-20'>
         <div className=''>
-            <h1 className='font-bold text-7xl'>Coming <br /> <span className='text-white'>Soon.</span></h1>
-            <p className='font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ut reprehenderit sed ipsa a, voluptas porro enim? Maiores, accusamus voluptatibus.</p>
+            <h1 className='font-bold text-black text-7xl'>Coming Soon.</h1>
+            <h1 className='font-bold text-6xl text-white'>Always<br /> <span className='text-black'>the Hottest Burger.</span></h1>
+            <p className='mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ut reprehenderit sed ipsa a, voluptas porro enim? Maiores, accusamus voluptatibus.</p>
+            <button className='bg-black text-white pl-20 pr-20 pt-4 pb-4 rounded-3xl mx-auto mt-4'>Read More</button>
         </div>
         <div>
             <img className='object-cover' src={burger} alt="burger" />
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Testimonial = () => {
   return (
     <div>
-        
+        <p className='text-center text-yellow font-bold'>Testimonials</p>
+        <h1 className='text-center font-bold text-black text-6xl'>Why Our Clients Choose Us.</h1>
     </div>
   )
 }
