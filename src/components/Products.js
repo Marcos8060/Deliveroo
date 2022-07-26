835d3b6a398ab2fed826b1719a0782cd474ebbe3
Marcos8060 <marcosgav80@gmail.com>
2022-07-26 18:31:03 +0300

    installed bootstrap icons

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,31 +2,44 @@ import React from 'react'
 import burger from '../assets/Images/burger3.png'
 import { GiShoppingBag } from 'react-icons/gi'
 
-
-import Slider from "react-slick";
-// REACT SLICK
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+// OWL CAROUSEL
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 
 
 const Products = () => {
 
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      };
+  const options = {
+    loop: true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+            center: true,
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    },
+    navText:[
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+}
 
   return (
     <div className='container'>
         <h3 className='text-center text-yellow font-bold mt-20'>Quick Pick</h3>
         <h1 className='text-center text-black font-bold text-6xl mb-20'>Popular Goods</h1>
         
-        <Slider {...settings}>
-           <div className='text-center transition'>
+        <OwlCarousel {...options}>
+           <div className='transition'>
               <img className='object-fit ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ...' src={burger} alt="burger" />
               <span className='text-center font-bold text-2xl'>Pepperoni</span>
               <div className='flex items-center justify-around'>
@@ -34,7 +47,8 @@ const Products = () => {
                 <GiShoppingBag className='text-center text-yellow text-2xl'/>
               </div>
             </div>
-            <div className='text-center'>
+
+            <div className=''>
               <img className='object-fit ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ...' src={burger} alt="burger" />
               <span className='text-center font-bold text-2xl'>Pepperoni</span>
               <div className="flex items-center justify-around">
@@ -42,7 +56,8 @@ const Products = () => {
                 <GiShoppingBag className='text-yellow text-2xl'/>
               </div>
             </div>
-            <div className='text-center'>
+
+            <div className=''>
               <img className='object-fit ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ...' src={burger} alt="burger" />
               <span className='text-center font-bold text-2xl'>Pepperoni</span>
               <div className="flex items-center justify-around">
@@ -50,7 +65,8 @@ const Products = () => {
                 <GiShoppingBag className='text-yellow text-2xl'/>
               </div>
             </div>
-            <div className='text-center'>
+
+            <div className=''>
               <img className='object-fit ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ...' src={burger} alt="burger" />
               <span className='text-center font-bold text-2xl'>Pepperoni</span>
               <div className="flex items-center justify-around">
@@ -58,7 +74,8 @@ const Products = () => {
                 <GiShoppingBag className='text-yellow text-2xl'/>
               </div>
             </div>
-        </Slider>
+
+        </OwlCarousel>
     </div>
   )
 }
