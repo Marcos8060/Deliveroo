5a8ec53478199d4515b60d5ea9302755a5a894f3
Marcos8060 <marcosgav80@gmail.com>
2022-07-27 00:27:18 +0300

    made minor changes

--- src/components/Cto.js
+++ src/components/Cto.js
@@ -10,20 +10,20 @@ const Cto = () => {
         <img className='object-cover w-7/12 h-screen' src={home} alt="" />
         <div className='bg-black text-white h-screen p-20'>
             <h1 className='font-bold text-5xl'>Sit at Home <br />  <span className='text-yellow'>We will take care of you.</span></h1><br />
-            <p className='text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis in accusantium esse beatae culpa!</p>
+            <p className='text-gray-400 font-thin'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis in accusantium esse beatae culpa!</p>
             <br /><br />
             <div className='flex items-center justify-between'>
               <div className='text-center'>
                <TbTruckDelivery className='text-yellow text-5xl text-center'/>
-               <h3>Fast Delivery in 1hr</h3>
+               <h3 className='font-thin'>Fast Delivery in 1hr</h3>
               </div>
               <div className='text-center'>
               <GoDeviceMobile className='text-yellow text-5xl'/>
-              <h3>Amazing Mobile App</h3>
+              <h3 className='font-thin'>Amazing Mobile App</h3>
               </div>
               <div className='text-center'>
               <FaMapMarkerAlt className='text-yellow text-5xl'/>
-              <h3>Wide Map Coverage</h3>
+              <h3 className='font-thin'>Wide Map Coverage</h3>
               </div>
             </div>
             <div className='flex justify-center items-center mt-20'>
--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -5,8 +5,8 @@ const Delivery = () => {
   return (
     <div className='flex items-center mx-auto p-20'>
         <div>
-         <h1 className='text-7xl font-bold'>Express Home <span className='text-yellow'>Delivery</span>.</h1>
-         <p className='font-semibold'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis obcaecati reprehenderit repellat saepe est quibusdam, nemo laboriosam cumque quo vitae!</p>
+         <h1 className='text-7xl font-bold mb-4'>Express Home <span className='text-yellow'>Delivery</span>.</h1>
+         <p className='font-normal'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis obcaecati reprehenderit repellat saepe est quibusdam, nemo laboriosam cumque quo vitae!</p>
         </div>
         <img className='object-fit w-7/12 mx-auto' src={rider} alt="" />
     </div>
