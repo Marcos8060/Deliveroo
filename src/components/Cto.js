80964b6e0b686573ae92af7167d227b356b61cf5
Marcos8060 <marcosgav80@gmail.com>
2022-07-28 00:58:36 +0300

    made a minor change

--- src/components/Cto.js
+++ src/components/Cto.js
@@ -26,7 +26,7 @@ const Cto = () => {
               <h3 className='font-thin'>Wide Map Coverage</h3>
               </div>
             </div>
-            <div className='flex justify-center items-center mt-20'>
+            <div className='flex justify-center items-center md:mt-20 mt-10'>
               <button className='bg-yellow text-black pl-10 pr-10 pt-4 pb-4 rounded-3xl font-semibold mb-4'>Read More</button>
             </div>
         </div>
