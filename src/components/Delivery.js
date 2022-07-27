cbbf7c612936c1e73c39c09ecf683005885e9a1b
Marcos8060 <marcosgav80@gmail.com>
2022-07-28 00:24:13 +0300

    added mobile responsiveness to delivery section

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -3,9 +3,9 @@ import rider from '../assets/Images/rider.png'
 
 const Delivery = () => {
   return (
-    <div className='flex items-center mx-auto p-20'>
+    <div className='flex md:items-center flex-col mx-auto md:p-20 p-4 mt-40'>
         <div>
-         <h1 className='text-7xl font-bold mb-4'>Express Home <span className='text-yellow'>Delivery</span>.</h1>
+         <h1 className='md:text-7xl text-4xl font-bold mb-4 text-center'>Express Home <span className='text-yellow'>Delivery</span>.</h1>
          <p className='font-normal'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis obcaecati reprehenderit repellat saepe est quibusdam, nemo laboriosam cumque quo vitae!</p>
         </div>
         <img className='object-fit w-7/12 mx-auto' src={rider} alt="" />
