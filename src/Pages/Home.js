b869c3dd14eabad3120e1ce16ce231eb5ed80dd3
Marcos8060 <marcosgav80@gmail.com>
2022-08-04 00:08:15 +0300

    changed the image of the hero section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
 import burger from '../assets/Images/burger2.png'
+import soda from '../assets/Images/soda.png'
 import Delivery from '../components/Delivery'
 import Products from '../components/Products'
 import Cto from '../components/Cto'
@@ -14,7 +15,7 @@ const Home = () => {
     <div className='home--background h-screen pt-4 mx-auto'>
         {/* <Navigation /> */}
         <div className='px-4 md:flex'>
-            <img className='object-cover md:w-8/12 mt-20' src={burger} alt="burger" />
+            <img className='object-cover md:w-8/12 mt-20' src={soda} alt="burger" />
             <h1 className='text-white md:text-8xl text-6xl text-center font-bold md:mt-56'>Delicious <span className='text-yellow'>Burgers.</span></h1>
         </div>
 
