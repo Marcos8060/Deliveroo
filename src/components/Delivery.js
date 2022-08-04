af72ac33c7d51d3eac52647abc401a3f3932b6ac
Marcos8060 <marcosgav80@gmail.com>
2022-08-04 17:59:35 +0300

    made a minor change

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -6,7 +6,7 @@ import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 const Delivery = () => {
   return (
-    <div className='md:flex md:items-center mx-auto md:p-20 md:mt-0 p-4 mt-60 grid'>
+    <div className='md:flex md:items-center mx-auto md:p-20 md:mt-0 p-4 mt-48 grid'>
         <div>
          <h1 className='md:text-7xl text-4xl md:font-bold mb-4 text-center'>Express Home <span className='text-yellow font-semibold'>Delivery</span>.</h1>
          <p className='font-normal'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis obcaecati reprehenderit repellat saepe est quibusdam, nemo laboriosam cumque quo vitae!</p>
