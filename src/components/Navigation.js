4a2677ad8bbd9396ecf3a5e047eccc0faae74242
Marcos8060 <marcosgav80@gmail.com>
2022-08-04 17:58:16 +0300

    made a minor change

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -24,16 +24,16 @@ const Navigation = () => {
                         w-11/12 rounded-2xl mt-2 right-4 md:p1-0 p1-9 transition-all duration-500
                           ease-in ${open ? 'top-20' : 'top-[-490px]'}`}>
             <li className="cursor-pointer md:ml-8 text-xl md:my-0 my-7 text-white">
-              <NavLink to="/artists" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'text-white no-underline'}>Home</NavLink>
+              <NavLink to="/artists" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'md:text-white text-black no-underline'}>Home</NavLink>
             </li>
             <li className="cursor-pointer md:ml-8 text-xl md:my-0 my-7">
-              <NavLink to="/fans" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'text-white no-underline'}>About</NavLink>
+              <NavLink to="/fans" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'md:text-white text-black no-underline'}>About</NavLink>
             </li>
             <li className="cursor-ppinter md:ml-8 text-xl md:my-0 my-7">
-              <NavLink to="/business" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'text-white no-underline'}>Services</NavLink>
+              <NavLink to="/business" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'md:text-white text-black no-underline'}>Services</NavLink>
             </li>
             <li className="cursor-pointer md:ml-8 text-xl md:my-0 my-7">
-              <NavLink to="/about" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'text-white no-underline'}>Coming Soon</NavLink>
+              <NavLink to="/about" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'md:text-white text-black no-underline'}>Coming Soon</NavLink>
             </li>
           </ul>
           <div className="flex items-center justify-end pt-4 md:pt-2">
