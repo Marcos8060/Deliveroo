8a3404516e0f2cc51b00d545a93d8540ab058551
Marcos8060 <marcosgav80@gmail.com>
2022-08-04 18:19:12 +0300

    made a minor change

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -8,8 +8,8 @@ const Delivery = () => {
   return (
     <div className='md:flex md:items-center mx-auto md:p-20 md:mt-0 p-4 mt-48 grid'>
         <div>
-         <h1 className='md:text-7xl text-4xl md:font-bold mb-4 text-center'>Express Home <span className='text-yellow font-semibold'>Delivery</span>.</h1>
-         <p className='font-normal'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis obcaecati reprehenderit repellat saepe est quibusdam, nemo laboriosam cumque quo vitae!</p>
+         <h1 className='md:text-6xl text-4xl md:font-bold mb-4 md:text-left text-center'>Express Home <span className='text-yellow font-semibold'>Delivery</span>.</h1>
+         <p className='font-normal md:text-left text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis obcaecati reprehenderit repellat saepe est quibusdam, nemo laboriosam cumque quo vitae!</p>
         </div>
            <img className='object-fit w-8/12 mx-auto' src={rider} alt="" />
     </div>
