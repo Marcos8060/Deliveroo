3d6641ccaaf6953dc87ca80cd80488da4037fb7d
Marcos8060 <marcosgav80@gmail.com>
2022-08-04 17:55:47 +0300

    added a hero button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,9 +14,13 @@ const Home = () => {
   return (
     <div className='home--background h-screen pt-4 mx-auto'>
         <Navigation />
-        <div className='px-4 md:flex'>
-            <img className='object-cover md:w-8/12 mt-20' src={soda} alt="burger" />
-            <h1 className='text-white md:text-8xl text-6xl text-center font-bold md:mt-56'>Delicious <span className='text-yellow'>Burgers.</span></h1>
+        <div className='px-4 md:flex md:justify-between md:items-center'>
+          <div className='mt-20 text-center'>
+            <h1 className='text-white md:text-6xl text-4xl font-bold mb-2'>Delicious Burgers at <span className='text-yellow'>Deliveroo</span>.</h1>
+            <p className='text-white font-thin'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolorum voluptate dolores, totam hic rerum a eligendi doloremque quis fugiat.</p>
+            <button className='bg-yellow text-black px-8 pt-4 pb-4 mt-4 font-semibold'>Buy Now</button>
+          </div>
+            <img className='object-cover md:w-6/12 mt-20' src={soda} alt="burger" />
         </div>
 
         <Delivery />
