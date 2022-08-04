64879cee36e12d03cfc04ba04719885f3fd2a07d
Marcos8060 <marcosgav80@gmail.com>
2022-08-04 17:22:59 +0300

    created a responsive mobile navbar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -13,7 +13,7 @@ import Footer from '../components/Footer'
 const Home = () => {
   return (
     <div className='home--background h-screen pt-4 mx-auto'>
-        {/* <Navigation /> */}
+        <Navigation />
         <div className='px-4 md:flex'>
             <img className='object-cover md:w-8/12 mt-20' src={soda} alt="burger" />
             <h1 className='text-white md:text-8xl text-6xl text-center font-bold md:mt-56'>Delicious <span className='text-yellow'>Burgers.</span></h1>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,49 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { GiShoppingBag } from 'react-icons/gi'
 import { HiUserCircle } from 'react-icons/hi'
-import { Link } from 'react-router-dom'
+import { Link, NavLink  } from 'react-router-dom'
+import { BiMenu } from 'react-icons/bi'
+import { AiOutlineClose } from 'react-icons/ai'
+import { BsFillPersonCheckFill } from "react-icons/bs";
 
 const Navigation = () => {
+  const [open,setOpen] = useState(false)
+
   return (
-    <div className='text-yellow h-14 w-11/12 mx-auto rounded-2xl px-4 md:flex justify-between items-center'>
-        <div>
-          <h1 className='font-bold text-4xl text-white'>Burger</h1>
+    <>
+      <div className="bg-yellow w-11/12 mx-auto rounded-2xl h-16 px-4 md:flex md:items-center justify-between">
+          <div onClick={()=>setOpen(!open)} className="text-3xl absolute cursor-pointer md:hidden block pt-3 z-10">
+            {
+              open ? <AiOutlineClose /> : <BiMenu />
+            }
+          </div>
+          <div className="text-3xl text-center md:pl-10 pl-14 pt-2 absolute">
+            <Link to="/" className="font-semibold no-underline text-black">Deliveroo</Link>
+          </div>
+          <ul className={`md:flex md:items-center md:pl-80 md:pb-2 pb-12 absolute md:static bg-yellow px-10
+                        w-11/12 rounded-2xl mt-2 right-4 md:p1-0 p1-9 transition-all duration-500
+                          ease-in ${open ? 'top-20' : 'top-[-490px]'}`}>
+            <li className="cursor-pointer md:ml-8 text-xl md:my-0 my-7 text-black">
+              <NavLink to="/artists" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'text-black no-underline'}>For Artists</NavLink>
+            </li>
+            <li className="cursor-pointer md:ml-8 text-xl md:my-0 my-7">
+              <NavLink to="/fans" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'text-black no-underline'}>For Fans</NavLink>
+            </li>
+            <li className="cursor-ppinter md:ml-8 text-xl md:my-0 my-7">
+              <NavLink to="/business" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'text-black no-underline'}>For Business</NavLink>
+            </li>
+            <li className="cursor-pointer md:ml-8 text-xl md:my-0 my-7">
+              <NavLink to="/about" className={({ isActive }) => isActive ? 'font-bold text-black no-underline' : 'text-black no-underline'}>About</NavLink>
+            </li>
+          </ul>
+          <div className="flex items-center justify-end pt-4 md:pt-2">
+            <Link className='text-black' to='/cart'>
+              <GiShoppingBag className='text-2xl mb-3 mr-10'/>
+            </Link>
+            <BsFillPersonCheckFill className="text-2xl mb-3" />
+          </div>
         </div>
-        <ul className='md:flex items-center justify-between'>
-            <li className='m-4 font-semibold'>
-                <Link to='/'>Home</Link>
-            </li>
-            <li className='m-4 font-semibold'>
-                <Link to='/about'>About</Link>
-            </li>
-            <li className='m-4 font-semibold'>
-                <Link to='/deliveries'>Deliveries</Link>
-            </li>
-        </ul>
-        <div className='flex items-center justify-between'>
-            <GiShoppingBag className='text-2xl m-4'/>
-            <HiUserCircle className='text-2xl m-4'/>
-        </div>
-    </div>
+    </>
   )
 }
 
