d29d5a1c6c7de6a6d3ffb3ce149f1cb78c98dc51
Marcos8060 <marcosgav80@gmail.com>
2022-08-06 20:58:36 +0300

    added mobile responsiveness for the detail page

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -1,28 +1,54 @@
 import React from 'react'
 import image from '../assets/Images/burger3.png'
 import Footer from '../components/Footer'
+import { FaTelegramPlane } from 'react-icons/fa'
+import { TbTruckDelivery } from 'react-icons/tb'
+import { GiCardPickup } from 'react-icons/gi'
 
 const Detail = () => {
   return (
     <>
-      <div className="flex">
-        <div className='flex w-7/12 mx-auto shadow-2xl my-20 p-10'>
+      <div className="md:flex grid px-4">
+        <div className='md:flex grid md:w-8/12 w-full mx-auto shadow-2xl md:my-20 my-10 p-10'>
             <div>
                 <img src={image} alt="" />
             </div>
             <div>
-                <small className='bg-yellow rounded p-1 font-semibold'>Official Store</small>
+                <small className='bg-yellow rounded p-1 font-thin'>Official Store</small>
                 <h1 className='my-2 text-2xl'>Pepperoni</h1>
-                <small className='bg-yellow rounded p-1 font-semibold'>Deal of the day</small>
+                <small className='bg-yellow rounded p-1 font-thin uppercase'>Deal of the day</small>
                 <hr className='my-4'/>
                 <h1 className='font-bold text-2xl'>Ksh 38</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda illo harum sunt temporibus, amet fugit quibusdam! Quos ratione eos veritatis!</p>
+                <p className='my-4 text-sm'>+ delivery from KSh 92 (free delivery if order above KSh 999) to CBD - UON/Globe/Koja/River Road</p>
+                <p className='my-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus distinctio a quod quasi doloremque quam voluptatibus similique eaque quidem non.</p>
                 <button className='bg-yellow uppercase p-3 w-full rounded text-white font-semibold my-2'>Add To Cart</button>
             </div>
         </div>
-        <div className='w-4/12 mx-auto shadow-2xl my-20 p-10'>
-            <h2 className='uppercase'>delivery & returns </h2>
-            <hr />
+        <div className='md:w-3/12 w-full mx-auto shadow-2xl md:my-20 my-4 p-4'>
+            <h2 className='uppercase font-semibold'>delivery & returns </h2>
+            <hr className='my-2'/>
+            <h3 className='flex items-center font-semibold text-sm'>Deliveroo
+                <FaTelegramPlane className='text-yellow'/>
+            </h3>
+            <small className='text-xs'>Eligible for free delivery for orders above Ksh 999 in Nairobi, Kiambu, and Mombasa (excluding medium and large items)</small>
+            <hr className='my-2'/>
+            <div className="flex mb-2">
+                <TbTruckDelivery className='text-7xl'/>
+                <div>
+                    <p>Door Delivery</p>
+                    <small>Delivery KSh 112 (free delivery if order above KSh 999)</small>
+                    <small>Delivery by 09 August when you order within next 21hrs 56mins</small>
+                </div>
+            </div>
+            <div className="flex">
+                <GiCardPickup className='text-7xl'/>
+                <div>
+                    <p>Pickup Station</p>
+                    <small>Delivery KSh 92 (free delivery if order above KSh 999)</small>
+                    <small>Delivery by 09 August when you order within next 21hrs 56mins</small>
+                </div>
+            </div>
+            <hr className='my-2'/>
         </div>
       </div>
       <Footer />
