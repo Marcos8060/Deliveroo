eb052122d147f5ad85999a8a8e18492ef1ca4b13
Marcos8060 <marcosgav80@gmail.com>
2022-08-06 22:30:15 +0300

    made a minor change in the products component

--- src/components/Products.js
+++ src/components/Products.js
@@ -45,8 +45,8 @@ const Products = () => {
 
           <div className="relative hover:cursor-pointer card--wrapper">
             <img className='h-80 w-80 rounded-2xl' src={burger} alt='burger' />
-            <div className="absolute bottom-0 left-0 right-0 px-2 py-4 h-1/2 bg-black rounded-bl-2xl rounded-br-2xl	flex flex-col justify-center items-center w-12/12 opacity-80 text-white">
-              <p className='text-3xl mb-10 font-thin'>Pepperoni</p>
+            <div className="absolute bottom-0 left-0 right-0 px-2 py-4 h-2/12 bg-black rounded-bl-2xl rounded-br-2xl	flex flex-col justify-center items-center w-12/12 opacity-80 text-white">
+              <p className='mb-2'>Pepperoni</p>
               <div className="flex items-center justify-between">
                 <p className='text-2xl mr-10'>$80</p>
                 <BsPlusCircle className='text-yellow text-4xl ml-10'/>
@@ -56,8 +56,8 @@ const Products = () => {
 
           <div className="relative hover:cursor-pointer card--wrapper">
             <img className='h-80 w-80 rounded-2xl' src={burger} alt='burger' />
-            <div className="absolute bottom-0 left-0 right-0 px-2 py-4 h-1/2 bg-black rounded-bl-2xl rounded-br-2xl	flex flex-col justify-center items-center w-12/12 opacity-80 text-white">
-              <p className='text-3xl mb-10 font-thin'>Pepperoni</p>
+            <div className="absolute bottom-0 left-0 right-0 px-2 py-4 h-2/12 bg-black rounded-bl-2xl rounded-br-2xl	flex flex-col justify-center items-center w-12/12 opacity-80 text-white">
+              <p className='mb-2'>Pepperoni</p>
               <div className="flex items-center justify-between">
                 <p className='text-2xl mr-10'>$80</p>
                 <BsPlusCircle className='text-yellow text-4xl ml-10'/>
@@ -67,8 +67,8 @@ const Products = () => {
 
           <div className="relative hover:cursor-pointer card--wrapper">
             <img className='h-80 w-80 rounded-2xl' src={burger} alt='burger' />
-            <div className="absolute bottom-0 left-0 right-0 px-2 py-4 h-1/2 bg-black rounded-bl-2xl rounded-br-2xl	flex flex-col justify-center items-center w-12/12 opacity-80 text-white">
-              <p className='text-3xl mb-10 font-thin'>Pepperoni</p>
+            <div className="absolute bottom-0 left-0 right-0 px-2 py-4 h-2/12 bg-black rounded-bl-2xl rounded-br-2xl	flex flex-col justify-center items-center w-12/12 opacity-80 text-white">
+              <p className='mb-2'>Pepperoni</p>
               <div className="flex items-center justify-between">
                 <p className='text-2xl mr-10'>$80</p>
                 <BsPlusCircle className='text-yellow text-4xl ml-10'/>
@@ -78,8 +78,8 @@ const Products = () => {
 
           <div className="relative hover:cursor-pointer card--wrapper">
             <img className='h-80 w-80 rounded-2xl' src={burger} alt='burger' />
-            <div className="absolute bottom-0 left-0 right-0 px-2 py-4 h-1/2 bg-black rounded-bl-2xl rounded-br-2xl	flex flex-col justify-center items-center w-12/12 opacity-80 text-white">
-              <p className='text-3xl mb-10 font-thin'>Pepperoni</p>
+            <div className="absolute bottom-0 left-0 right-0 px-2 py-4 h-2/12 bg-black rounded-bl-2xl rounded-br-2xl	flex flex-col justify-center items-center w-12/12 opacity-80 text-white">
+              <p className='mb-2'>Pepperoni</p>
               <div className="flex items-center justify-between">
                 <p className='text-2xl mr-10'>$80</p>
                 <BsPlusCircle className='text-yellow text-4xl ml-10'/>
@@ -89,8 +89,8 @@ const Products = () => {
 
           <div className="relative hover:cursor-pointer card--wrapper">
             <img className='h-80 w-80 rounded-2xl' src={burger} alt='burger' />
-            <div className="absolute bottom-0 left-0 right-0 px-2 py-4 h-1/2 bg-black rounded-bl-2xl rounded-br-2xl	flex flex-col justify-center items-center w-12/12 opacity-80 text-white">
-              <p className='text-3xl mb-10 font-thin'>Pepperoni</p>
+            <div className="absolute bottom-0 left-0 right-0 px-2 py-4 h-2/12 bg-black rounded-bl-2xl rounded-br-2xl	flex flex-col justify-center items-center w-12/12 opacity-80 text-white">
+              <p className='mb-2'>Pepperoni</p>
               <div className="flex items-center justify-between">
                 <p className='text-2xl mr-10'>$80</p>
                 <BsPlusCircle className='text-yellow text-4xl ml-10'/>
