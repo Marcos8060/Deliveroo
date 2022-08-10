5b1a031ca29d4ae8185eca1f06b116fcb456e06e
Marcos8060 <marcosgav80@gmail.com>
2022-08-10 16:15:34 +0300

    mocked up the signup form

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Footer from '../components/Footer'
+
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+        <div className='p-4 mb-20'>
+            <div className='shadow-2xl md:w-4/12 w-full mx-auto p-4 mt-20 rounded'>
+                <form className='flex flex-col items-center justify-center w-full'>
+                    <input className='block my-4 p-2 w-11/12 mx-auto border rounded focus:border-none' placeholder='username...' />
+                    <input className='block my-4 p-2 w-11/12 mx-auto border rounded' type="text" placeholder='email...' />
+                    <input className='block my-4 p-2 w-11/12 mx-auto border rounded' type="text" placeholder='password...' />
+                    <button className='bg-black text-white p-2 rounded w-11/12 mx-auto'>Sign Up</button>
+                </form>
+            </div>
+        </div>
+        <Footer />
+    </>
   )
 }
 
