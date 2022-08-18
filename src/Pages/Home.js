aa7518ba9901eed039be7d005b8ad448ecdf8597
Marcos8060 <marcosgav80@gmail.com>
2022-08-18 13:10:21 +0300

    created the hero component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,21 +7,15 @@ import Cto from '../components/Cto'
 import Soon from '../components/Soon'
 import Testimonial from '../components/Testimonial'
 import Footer from '../components/Footer'
+import Hero from '../components/Hero'
 
 
 const Home = () => {
   return (
-    <div className='home--background h-screen pt-4 mx-auto'>
-        <Navigation />
-        <div className='px-4 md:flex md:justify-between md:items-center'>
-          <div className='mt-20 text-center'>
-            <h1 className='text-white md:text-6xl text-4xl font-bold mb-2'>Delicious Burgers at <span className='text-yellow'>Deliveroo</span>.</h1>
-            <p className='text-white font-thin'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolorum voluptate dolores, totam hic rerum a eligendi doloremque quis fugiat.</p>
-            <button className='bg-yellow text-black px-8 pt-4 pb-4 mt-4 font-semibold'>Buy Now</button>
-          </div>
-            <img className='object-cover md:w-6/12 mt-20' src={soda} alt="burger" />
-        </div>
-
+    <>
+        {/* <Navigation /> */}
+        <Hero />
+        
         <Delivery />
 
         <Products />
@@ -33,7 +27,7 @@ const Home = () => {
         <Testimonial />
 
         <Footer />
-    </div>
+    </>
   )
 }
 
