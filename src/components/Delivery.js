cc8457c6e401a0a2d1da20ad31aa2890c488cbb2
Marcos8060 <marcosgav80@gmail.com>
2022-08-21 13:54:53 +0300

    updated the hero section

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -4,7 +4,7 @@ import "animate.css/animate.min.css";
 
 const Delivery = () => {
   return (
-    <div className='md:flex md:items-center mx-auto md:p-20 md:mt-0 p-4 mt-48 grid'>
+    <div className='md:flex md:items-center mx-auto md:p-20 md:mt-0 p-4 md:mt-32 grid'>
         <div>
          <h1 className='md:text-4xl text-4xl md:font-bold mb-4 md:text-left text-center'>Express Home <span className='text-yellow font-semibold'>Delivery</span>.</h1>
          <p className='font-normal md:text-left text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis obcaecati reprehenderit repellat saepe est quibusdam, nemo laboriosam cumque quo vitae!</p>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,11 +11,12 @@ const Hero = () => {
     <>
       <div className="bg-white h-100v">
         <Navigation />
-        <div className="flex items-center justify-between">
-          <div className="w-1/2 p-2">
-            {/* <h1>We make the double Cheesy Burger</h1> */}
+        <div className="md:flex items-center justify-between">
+          <div className="hidden md:block md:w-1/2 w-full mx-auto p-2 mt-20">
+            <h1 className="mt-40v">We make the double <br /> Cheesy Burger</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae nihil delectus odio voluptatum magni provident dignissimos fuga maiores veritatis architecto.</p>
           </div>
-          <div className="w-1/2 p-2 bg-yellow rounded flex overflow-hidden">
+          <div className="md:w-1/2 w-full p-2 bg-yellow flex items-center overflow-hidden">
                 <img className="h-90v mt-12" src={burger} alt="burger" />
                 <img className="h-40v my-40" src={burger1} alt="burger" />
           </div>
