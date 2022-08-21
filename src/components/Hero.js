5507d69d41438db63835c820ce01787a73b2e38d
Marcos8060 <marcosgav80@gmail.com>
2022-08-21 12:52:13 +0300

    made changes in the hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,14 +6,12 @@ const Hero = () => {
 
   return (
     <>
-      <div className="flex items-center mt-20 bg-grey">
-        <div className='px-10'>
-          <h1 className='font-bold'>We make the double Cheesy Burger</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore cumque temporibus iste sit ut vitae deleniti accusamus fugit odit odio quisquam molestias quis, expedita fuga assumenda. Animi nisi tenetur architecto.</p>
-          
+      <div className="flex items-center justify-between">
+        <div>
+          <h1>We make the double Cheesy Burger</h1>
         </div>
-        <div className=''>
-          <img src={burger} alt="" />
+        <div>
+          <img src={burger} alt="burger"/>
         </div>
       </div>
     </>
