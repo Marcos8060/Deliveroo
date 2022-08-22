2355326d34d0af046086165f67a55e519592f119
Marcos8060 <marcosgav80@gmail.com>
2022-08-22 22:43:40 +0300

    added a hero button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,13 +13,14 @@ const Hero = () => {
         <div className="md:flex items-center justify-between">
           <div className="hidden md:block md:w-1/2 w-full mx-auto pl-10 mt-10">
             <h1 className="mt-40v text-6xl font-black">
-              The Double <br /> Decker
+              <span className="text-4xl">Try Our new</span> <br /> Double Decker
             </h1>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Repudiandae nihil delectus odio voluptatum magni provident
               dignissimos fuga maiores veritatis architecto.
             </p>
+            <button className="bg-yellow text-white p-2 uppercase">Order Now</button>
           </div>
           <div className="md:w-1/2 w-full p-2 bg-yellow rounded-bl-3xl flex items-center overflow-hidden">
             <img className="h-90v mt-12" src={burger} alt="burger" />
