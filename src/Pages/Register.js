d76b56cc16e16390735fb25c25e855149ef3046b
Marcos8060 <marcosgav80@gmail.com>
2022-08-26 17:23:47 +0300

    added the handleRegister function

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,76 +1,98 @@
-import React,{useState} from 'react'
-import Footer from '../components/Footer'
-import { register } from '../axios'
-import axios from 'axios'
-import { useSelector,useDispatch } from 'react-redux'
-import { clearMessage } from '../redux/features/message/messageSlice'
+import React, { useEffect, useState } from "react";
+import Footer from "../components/Footer";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { clearMessage } from "../redux/features/message/messageSlice";
+import { register } from '../redux/features/auth/authSlice'
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
 
 
 const Register = () => {
-    const dispatch = useDispatch();
-    const [formData,setFormData] = useState({
-        username:'',
-        email: '',
-        password:'',
+  const dispatch = useDispatch();
+  const history = useNavigate();
+  const [successfull, setSuccessfull] = useState(false);
+  const { message } = useSelector((state) => state.message);
+
+  useEffect(() => {
+    dispatch(clearMessage());
+  }, [dispatch]);
+
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  // validation  schema
+  const validationSchema = Yup.object().shape({
+    username: Yup.string()
+      .test(
+        "len",
+        "The username must be between 3 and 20 characters.",
+        (val) =>
+          val && val.toString().length >= 3 && val.toString().length <= 20
+      )
+      .required("This field is required!"),
+    email: Yup.string()
+      .email("This is not a valid email.")
+      .required("This field is required!"),
+    password: Yup.string()
+      .test(
+        "len",
+        "The password must be between 6 and 40 characters.",
+        (val) =>
+          val && val.toString().length >= 6 && val.toString().length <= 40
+      )
+      .required("This field is required!"),
+  });
+
+//   HandleRegister Function
+  const HandleRegister = (formValue) =>{
+    const { username, email, password } = formValue;
+    setSuccessfull(false);
+    dispatch(register({ username, email,password}))
+    .unwrap()
+    .then((res) =>{
+        setSuccessfull(true);
     })
-
-
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name] : e.target.value
-         })
-    }
-
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        
-        axios.post(register,{
-            username: formData.username,
-            email: formData.email,
-            password: formData.password
-        })
-        .then((res) =>{
-            console.log(res.data)
-        })
-        .catch((err) =>{
-            console.log(err.response.status)
-        })
-    }
-
+    .catch((err) =>{
+        setSuccessfull(false);
+    })
+  }
 
   return (
     <>
-        <div className='p-4 mb-20'>
-            <div className='shadow-2xl md:w-4/12 w-full mx-auto p-4 mt-20 rounded'>
-                <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center w-full'>
-                    <input 
-                      name='username' 
-                      className='block my-4 p-2 w-11/12 mx-auto border rounded focus:border-none' 
-                      placeholder='username...'
-                      onChange={handleChange}
-                       />
-                    <input 
-                      name='email' 
-                      className='block my-4 p-2 w-11/12 mx-auto border rounded' 
-                      type="text" 
-                      placeholder='email...' 
-                      onChange={handleChange}
-                      />
-                    <input 
-                      name='password' 
-                      className='block my-4 p-2 w-11/12 mx-auto border rounded' 
-                      type="text" 
-                      placeholder='password...' 
-                      onChange={handleChange}
-                      />
-                    <button className='bg-black text-white p-2 rounded w-11/12 mx-auto'>Sign Up</button>
-                </form>
-            </div>
+      <div className="p-4 mb-20">
+        <div className="shadow-2xl md:w-4/12 w-full mx-auto p-4 mt-20 rounded">
+          <form className="flex flex-col items-center justify-center w-full">
+            <input
+              name="username"
+              className="block my-4 p-2 w-11/12 mx-auto border rounded focus:border-none"
+              placeholder="username..."
+            />
+            <input
+              name="email"
+              className="block my-4 p-2 w-11/12 mx-auto border rounded"
+              type="text"
+              placeholder="email..."
+            />
+            <input
+              name="password"
+              className="block my-4 p-2 w-11/12 mx-auto border rounded"
+              type="text"
+              placeholder="password..."
+            />
+            <button className="bg-black text-white p-2 rounded w-11/12 mx-auto">
+              Sign Up
+            </button>
+          </form>
         </div>
-        <Footer />
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
